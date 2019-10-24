5bf6fb80af1a7c5cd4a671f2ca57d6aacb9b764d
Kumar <212304420@logon.ds.ge.com>
2019-10-24 15:23:48 -0400

    Minor edits to docker compose yml file and env settings

--- ASKE-Service/env.sh
+++ ASKE-Service/env.sh
@@ -2,6 +2,7 @@
 # Environment variables for the DBN service for ASKE
 
 export DBN_PORT=5000
+export JSON_GEN_PORT=46000
 export FS_PATH=/c/Users/200019210/Documents/2019/Projects/ASKE-TA2/DBN-try-docker-local/
 export COMPOSE_FILE=compose-aske.yml
 
