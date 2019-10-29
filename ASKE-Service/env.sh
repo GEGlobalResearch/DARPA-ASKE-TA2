09b74ab607ab0de44cf8238c020332b35bb984ec
Alfredo Gabaldon <agabaldon@gmail.com>
2019-10-29 13:27:04 -0400

    change default port for DBN service

--- ASKE-Service/env.sh
+++ ASKE-Service/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Environment variables for the DBN service for ASKE
 
-export DBN_PORT=5000
+export DBN_PORT=5050
 export JSON_GEN_PORT=46000
-export FS_PATH=/c/Users/200019210/Documents/2019/Projects/ASKE-TA2/DBN-try-docker-local/
+export FS_PATH=
 export COMPOSE_FILE=compose-aske.yml
 
