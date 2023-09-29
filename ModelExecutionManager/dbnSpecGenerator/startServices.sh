264ab60f1ed4b7b1db1b9b2afbc7753d1515323a
Alfredo Gabaldon <agabaldon@gmail.com>
2023-09-29 15:37:37 -0400

    add create logs dir and kill current process

--- ModelExecutionManager/dbnSpecGenerator/startServices.sh
+++ ModelExecutionManager/dbnSpecGenerator/startServices.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+if [ ! -d "logs" ]; 
+then
+     echo "logs directory does not exist - creating it"
+     mkdir logs
+fi
+
+PID=`pgrep -f dbnSpecGenerator`
+if [ -n "${PID}" ];
+then
+   echo "killing running dbn json service process"
+   pkill -f dbnSpecGenerator
+fi
+
+echo "Restarting dbn json service"
 java -jar ./target/dbnSpecGenerator-1.0-SNAPSHOT.jar --server.port=46000 > ./logs/service.log 2>&1 &
